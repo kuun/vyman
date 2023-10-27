4a207b7509e20e7e359cf1be866e4b57fcab7ad9
kuun <kuunwang@gmail.com>
2023-10-27 18:13:46 +0800

    support to build sourcemap

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    sourcemap: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
