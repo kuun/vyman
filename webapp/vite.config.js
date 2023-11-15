878b94c6d183cb7e2399a21ea242195a4a21b6f3
wx <18621760020@163.com>
2023-11-15 15:38:18 +0800

    添加vite代理，可本地调试web页面代码

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -15,5 +15,25 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  //本地开发时打开可以远程调试前端代码，调试时改为自己的服务器地址和端口即可
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.237:8000/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/ui': {
+        target: 'http://192.168.0.237:8000/ui',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ui/, '')
+      },
+      '/assets': {
+        target: 'http://192.168.0.237:8000/assets',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/assets/, '')
+      }
+    }
   }
 })
